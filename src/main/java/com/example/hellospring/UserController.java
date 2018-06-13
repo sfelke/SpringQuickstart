b6ad1c49e1df070cf0c3ecadb6fe4d39e556bc30
Stephan Felke <s.felke@itsonix.eu>
2018-06-13 17:39:55 +0200

    POST and GET request for user

--- src/main/java/com/example/hellospring/UserController.java
+++ src/main/java/com/example/hellospring/UserController.java
@@ -1,14 +1,17 @@
 package com.example.hellospring;
 
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class UserController {
 
     private GreetingHandler greetingHandler;
+
+    private List<User> users = new ArrayList<>();
+
 
     public UserController(GreetingHandler greetingHandler){
         this.greetingHandler = greetingHandler;
@@ -23,4 +26,15 @@ public class UserController {
     public String helloUserParam(@RequestParam String user){
         return "hello " + user;
     }
+
+    @RequestMapping(value = "/user", method = RequestMethod.POST)
+    public void createUser(@RequestBody User user){
+        users.add(user);
+    }
+
+    @RequestMapping(value = "/user")
+    public List<User> createUser(){
+        return users;
+    }
+
 }
