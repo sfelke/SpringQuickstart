f8d46afb0ac2aba520af752475171ee97e673aee
Stephan Felke <s.felke@itsonix.eu>
2018-06-18 09:56:11 +0200

    added configuration for greetinghandler

--- src/main/java/com/example/hellospring/GreetingHandler.java
+++ src/main/java/com/example/hellospring/GreetingHandler.java
@@ -1,12 +1,16 @@
 package com.example.hellospring;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class GreetingHandler {
 
+    @Value("${user.standardGreeting}")
+    private String standardGreeting;
+
     public String sayHi(){
-        return "HI";
+        return this.standardGreeting;
     }
 
 }
